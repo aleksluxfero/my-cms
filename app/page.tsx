679d9fd17bd838c8f2814099abbf7ce406728edd
Lux Fero <aleks.luxfero@gmail.com>
2025-03-18 13:01:36 +0500

    Finalize ESLint config with TypeScript, React Hooks, Next.js, and Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
-  const unused = 1; // Неиспользуемая переменная
   return (
     <div>
-      Hello World <a href="/about">Link</a>
+      Hello World <a href="/about">Привет</a>
     </div>
   );
 }
